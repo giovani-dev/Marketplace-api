292e240ef7de0006414ab7d45e0a07f098e9b658
giovani <giovani@leaderweb.com.br>
2021-02-03 12:43:55 -0300

    Feat: added initial bash to run the project

--- run.sh
+++ run.sh
@@ -1,13 +1,29 @@
-cd MarketPlace/
-docker build -t api_market_place .
-cd ../
-docker-compose build
+#!/bin/bash
 
-docker run api_market_place -d
+echo
+echo "------------------------- Iniciando configuraçao de virtualenv -------------------------"
+echo
+
+pip install virtualenv
+virtualenv venv/market_place -p python3.8
+source venv/market_place/bin/activate
+python -m pip install --upgrade pip
+cd MarketPlace/
+pip install -r requirements.txt
+
+echo
+echo "------------------------- Iniciando configuraçao do Docker -------------------------"
+echo
+
+cd ../
+docker-compose down
+docker-compose build
 docker-compose up -d
 
-echo 
-echo "====================================="
-echo "PROJETO INICIADO COM SUCESSO!"
-echo "====================================="
 echo
+echo "------------------------- Iniciando projeto -------------------------"
+echo
+
+cd MarketPlace/
+python manage.py migrate
+python manage.py runserver
